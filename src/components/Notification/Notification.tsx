6a933353fd47e1d986b1704e0913cae9245493a1
Siamak Ahmadi` <mrsiamak.dev@gmail.com>
2023-03-05 16:31:44 +0330

    First

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface NotificationProps{
+    content:String;
+}
+
+const Notification = (props:NotificationProps)=>{
+    return <div>Hello this is a notification</div>
+}
+
+export default Notification;
